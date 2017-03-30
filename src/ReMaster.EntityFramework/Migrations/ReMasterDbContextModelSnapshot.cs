0d3b4ca9e53ef1c871b627bf4323356c098f178a
Karina <karina.skorka@gmail.com>
2017-03-30 23:00:05 +0200

    Migrating solution to VS2017.

--- src/ReMaster.EntityFramework/Migrations/ReMasterDbContextModelSnapshot.cs
+++ src/ReMaster.EntityFramework/Migrations/ReMasterDbContextModelSnapshot.cs
@@ -13,7 +13,7 @@ namespace ReMaster.EntityFramework.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("ProductVersion", "1.1.1")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("ReMaster.Core.Model.Company.Company", b =>
